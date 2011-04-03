d690a4cbe919cd0a0cdf2c0829aa0b57606497a4
Fabio Scognamiglio <fascox@me.com>
2011-04-04 00:49:49 +0200

    Added a site layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,15 @@
 class PagesController < ApplicationController
+
   def home
+      @title ="Home"
   end
 
   def contact
+     @title ="Contacts"
+  end
+  
+  def about
+     @title ="About"
   end
 
 end
