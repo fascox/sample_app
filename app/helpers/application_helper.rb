0cb2367b22b963ad2f16bee077aec28d9f6ca9cb
Fabio Scognamiglio <fascox@me.com>
2011-04-04 21:01:19 +0200

    Added blueprint stylesheet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  
+  def f_title
+    
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if @title.nil?       
+        f_title = base_title
+    else
+        f_title = "#{base_title} | #{@title}"
+    end    
+  end  
+  
 end
